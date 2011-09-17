f043b672d4d364f4ed5cccf52e5909a0b40716fb
Egor Egorov <me@egorfine.com>
2011-09-17 21:09:41 +0300

    working days

--- test/ExtraDateTest.js
+++ test/ExtraDateTest.js
@@ -1,20 +1,5 @@
 require('../lib/ExtraDate');
 
-/*
-exports['check parse mm_dd_yyyy'] = function(test) {
-	test.expect(6);
-	var da = Date.parse_mm_dd_yyyy("01/30/2010");
-	da.set_hh_mm_ss("14:53:02");
-	test.equal(da.getFullYear(), 2010, "year");
-	test.equal(da.getMonth(), 0, "month");
-	test.equal(da.getDate(), 30, "day");
-	test.equal(da.getHours(), 14, "hours");
-	test.equal(da.getMinutes(), 53, "minutes");
-	test.equal(da.getSeconds(), 2, "seconds");
-	test.done();
-};
-*/
-
 exports['check parse daystamp'] = function(test) {
 	test.expect(6);
 	
---
+++ test/WorkingDaysTest.js
@@ -0,0 +1,58 @@
+util = require('util');
+require('../lib/ExtraDate');
+require('../lib/WorkingDays');
+
+exports['check nyse working days'] = function(test) {
+	process.env.TZ='America/New_York';
+	
+	test.expect(6);
+	
+	var day;
+
+	day = Date.parseDaystamp('20110903'); // saturday
+	test.ok(!WorkingDays.isNyseHoliday(day));
+	test.ok(WorkingDays.isWeekend(day));
+
+	day = Date.parseDaystamp('20110904'); // sunday
+	test.ok(!WorkingDays.isNyseHoliday(day));
+	test.ok(WorkingDays.isWeekend(day));
+
+	day = Date.parseDaystamp('20110905'); // holiday
+	test.ok(WorkingDays.isNyseHoliday(day));
+
+	day = Date.parseDaystamp('20110906'); // working day
+	test.ok(!WorkingDays.isNyseHoliday(day));
+
+	test.done();
+}
+
+exports['check nyse prev/next day'] = function(test) {
+	process.env.TZ='America/New_York';
+	
+	test.expect(6);
+	
+	var day;
+
+	// next
+	day = Date.parseDaystamp('20110906'); // 20110905 is a holiday, 4 and 3 is weekend
+	test.equal(WorkingDays.prevNyseDay(day).daystamp(), '20110902');
+	
+	day = Date.parseDaystamp('20110902'); 
+	test.equal(WorkingDays.prevNyseDay(day).daystamp(), '20110901');
+
+	day = Date.parseDaystamp('20110919'); 
+	test.equal(WorkingDays.prevNyseDay(day).daystamp(), '20110916');
+
+	// prev
+	day = Date.parseDaystamp('20110902'); // 20110905 is a holiday, 4 and 3 is weekend
+	test.equal(WorkingDays.nextNyseDay(day).daystamp(), '20110906');
+	
+	day = Date.parseDaystamp('20110901'); 
+	test.equal(WorkingDays.nextNyseDay(day).daystamp(), '20110902');
+
+	day = Date.parseDaystamp('20110916'); 
+	test.equal(WorkingDays.nextNyseDay(day).daystamp(), '20110919');
+	
+	test.done();
+}
+
