e81f6542ab4e10c5ac33cd8b7e79cd93db9a1f88
Egor Egorov <me@egorfine.com>
2011-10-11 17:26:29 +0300

    ExtraLog landed

---
+++ ExtraLog.js
@@ -0,0 +1,50 @@
+require('./ExtraNumber');
+require('./ExtraDate');
+
+var formatRegExp = /%[sdjpTD%]/g;
+
+
+function format(f) {
+	if (typeof f !== 'string') {
+		var objects = [];
+		for (var i = 0; i < arguments.length; i++) {
+			objects.push(inspect(arguments[i]));
+		}
+		return objects.join(' ');
+	}
+
+	var i = 1;
+	var args = arguments;
+	var len = args.length;
+	var str = String(f).replace(formatRegExp, function(x) {
+		if (i >= len) return x;
+		switch (x) {
+			case '%D':return Date.parseUnixtime(args[i++]).toFormat('YYYYMMDD');
+			case '%T':return Date.parseUnixtime(args[i++]).toFormat('HH24:MI:SS');
+			case '%s':return String(args[i++]);
+			case '%d':return Number(args[i++]);
+			case '%p':return Number(args[i++]).humanReadablePrice();
+			case '%j':return JSON.stringify(args[i++]);
+			case '%%':return '%';
+			default:
+				return x;
+		}
+	});
+	for (var x = args[i]; i < len; x = args[++i]) {
+		if (x === null || typeof x !== 'object') {
+			str += ' ' + x;
+		} else {
+			str += ' ' + inspect(x);
+		}
+	}
+	return str;
+}
+
+	
+module.exports = {
+	format: format,
+	
+	log: function() {
+		process.stdout.write(format.apply(this, arguments) + '\n');
+	}
+};
---
+++ test/ExtraLogTest.js
@@ -0,0 +1,21 @@
+require('node-date');
+ExtraLog = require('../ExtraLog');
+
+exports['price format'] = function(test) {
+	test.expect(4);
+	
+	var d = new Date();
+	d.setHours(13,23,41,00);
+	d.setMonth(11, 29);
+	d.setYear(2011);
+	
+	test.equal(ExtraLog.format('%s %d', 'string', 1), 'string 1');
+	test.equal(ExtraLog.format('%p', 10000), '1.00');
+	test.equal(ExtraLog.format('%T', d.unixtime()), '13:23:41');
+	test.equal(ExtraLog.format('%D', d.unixtime()), '20111229');
+	
+	ExtraLog.log("%p", 10000);
+	
+	test.done();
+};
+
