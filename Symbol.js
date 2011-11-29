1a87f93c3764ef3faba54387b16e6acbf2fba369
Egor Egorov <me@egorfine.com>
2011-11-29 18:28:12 +0200

    documentation

--- Symbol.js
+++ Symbol.js
@@ -1,7 +1,33 @@
 fs = require('fs');
 path = require('path');
 
-Symbol = function(dbPath, symbol) {
+/**
+
+Class that represents one ticker storage in the ticks database. Essentially this is just a wrapper around a 
+list of *.ticks files with handy access methods. 
+
+@param {String} dbPath path to ticks database.
+@param {String} symbol symbol name, must be uppercase.
+
+Tick files list is generated from <code>dbPath/symbol/*.ticks</code>.
+
+@return {Symbol} 
+
+Example: 
+
+	var symbol = new Symbol('/home/tickers', 'AAPL');
+	if (!symbol.load()) {
+		console.log("Oops?");
+		return;
+	}
+
+	var daystamp;
+	while ((daystamp=symbol.next())) {
+		console.log("Day: %s", daystamp);
+	}
+*/
+
+function Symbol(dbPath, symbol) {
 	this.dbPath = dbPath;
 	this.symbol = symbol;
 	this.days = [];
@@ -15,6 +41,14 @@ Symbol = function(dbPath, symbol) {
 	this._loadMeta();
 }
 
+/**
+
+Set active flag for the ticker. This flag is used by downloader to skip download this ticker from 
+IQFeed if the flag is set. Defaults to false, of course. Don't forget to call <code>.save()</code> after
+setting the flag.
+
+ */
+
 Symbol.prototype.setIsActive = function(isActive) {
 	if (isActive) {
 		delete this.meta.isInactive;
@@ -23,9 +57,22 @@ Symbol.prototype.setIsActive = function(isActive) {
 	}
 }
 
+/**
+
+Get active flag for the ticker. 
+
+*/
+
 Symbol.prototype.isActive = function() {
 	return this.meta.isInactive ? false : true;
 }
+
+/**
+
+Load meta information from <code>dbPath/symbol/meta.json</code>.
+
+*/
+
 
 Symbol.prototype._loadMeta = function() {
 	if (path.existsSync(this._metaFilename)) {
@@ -40,9 +87,21 @@ Symbol.prototype._loadMeta = function() {
 	}
 }
 
+/**
+
+Save updated information for the ticker. Now this is just the meta information that contains active flag.
+
+*/
+
 Symbol.prototype.save = function() {
 	this._saveMeta();
 }
+
+/**
+
+Save meta information to <code>dbPath/symbol/meta.json</code>.
+
+*/
 
 Symbol.prototype._saveMeta = function() {
 	if (Object.keys(this.meta).length <= 0) {
@@ -56,6 +115,15 @@ Symbol.prototype._saveMeta = function() {
 	}
 	fs.renameSync(this._metaFilename+'.tmp', this._metaFilename);
 }
+
+/** 
+
+Load the ticker from dbpath. Essentially does load the list of all days available for the ticker and 
+the meta information.
+
+@return {Boolean} if loaded successfully.
+
+ */
 
 Symbol.prototype.load = function() {
 	this._loadMeta();
@@ -74,35 +142,94 @@ Symbol.prototype.load = function() {
 	}, this);
 	
 	this.days = files.map(function(filename) {
-		return filename.substr(0,8)
+		return filename.substr(0,8);
 	}, this);
 	
 	return true;
 }
 
+/** 
+
+Single-step iterator over all days. 
+
+@param {Function} cb callback.
+@param context set <code>this</code> to this.
+
+Callback function will receive one argument: the daystamp, string.
+
+*/
+
 Symbol.prototype.forEachDay = function(cb, context) {
 	this.days.forEach(cb, context);
 }
+
+/** 
+
+Rewind current iterator position to the beginning.
+
+*/
 
 Symbol.prototype.rewind = function() {
 	this.dayPosition=0;
 }
 
+/** 
+
+Get the next day from list of days. 
+
+@return {String} daystamp.
+
+*/
+
 Symbol.prototype.nextDay = function() {
 	return this.days[this.dayPosition++];
 }
+
+/** 
+
+Get the first day from list of days. 
+
+@return {String} daystamp.
+
+*/
 
 Symbol.prototype.firstDay = function() {
 	return this.days[0];
 }
 
+/** 
+
+Get the last day from list of days. 
+
+@return {String} daystamp.
+
+*/
+
 Symbol.prototype.lastDay = function() {
 	return this.days[this.days.length-1];
 }
 
+/** 
+
+Get the count of available days.
+
+@return {String} count of days.
+
+*/
+
 Symbol.prototype.count = function() {
 	return this.days.length;
 }
+
+/** 
+
+Check if this daystamp exists for this ticker.
+
+@param {String} daystamp day to look for.
+
+@return {Boolean} 
+
+*/
 
 Symbol.prototype.dayExists = function(daystamp) {
 	return this.days.indexOf(daystamp)>=0;
